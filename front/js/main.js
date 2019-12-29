2bcc23795df5b3d4484e3480e48887aab1f408f5
ffamilyfriendly <jonathan.hermin@skola.sala.se>
2019-12-29 20:57:42 +0100

    unminified scrips due to errors

--- front/js/main.js
+++ front/js/main.js
@@ -1,1 +1,41 @@
-const parseCookies = cs => Object.fromEntries(cs.split("; ").map(x => x.split("="))), cookieEncode = val => "string" == typeof val ? val.replace(/\=/g, "*") : val, cookieDecode = val => "string" == typeof val ? val.replace(/\*/g, "=") : val; window.setting = { set: (key, value) => { if (window.setting.exists("cookies") || (document.cookie = "cookies=false; max-age=31536000;"), "cookies" !== key && "false" === parseCookies(document.cookie).cookies) throw new Error("cookies forbidden"); document.cookie = `=${cookieEncode(value)}; max-age=31536000;` }, exists: key => { const settings = parseCookies(document.cookie); return void 0 !== settings[key] }, remove: key => { document.cookie = `=; expires=Thu, 01 Jan 1970 00:00:01 GMT;` }, get: key => key ? cookieDecode(parseCookies(document.cookie)[key]) : parseCookies(document.cookie) }, window.popup = (type, data) => { const html = `<div class="centerDiv popup popup-">\n    <h1 class="center"></h1>\n    <p></p>\n    <button onclick='$(this).parent().remove()'>Dismiss</button>\n    </div>`; $("body").prepend(html) }, $(document).ready(() => { window.setting.exists("cookies") && "false" != window.setting.get("cookies") || $("body").prepend('<div id="cookie_header" class="banner center"><span id="notice-text">this site uses cookies to store settings. By continuing to use the site you agree to the <a href="/terms">Terms of service</a></span><hr class="mobile"><button onclick="window.setting.set(\'cookies\',true); $(\'#cookie_header\').remove()" id="btn-dismiss" class="btn-yellow">dismiss</button></div>') }), window.onerror = err => { window.popup("error", `<br><a class="btn-red" href="https://stackoverflow.com/search?q=">Stack overflow</a>`) };
+const parseCookies = cs => Object.fromEntries(cs.split('; ').map(x => x.split('=')))
+const cookieEncode = val => typeof val == "string" ? val.replace(/\=/g,"*") : val
+const cookieDecode = val => typeof val == "string" ? val.replace(/\*/g,"=") : val
+
+window.setting = {
+    set: (key, value) => {
+        if(!window.setting.exists("cookies")) document.cookie = "cookies=false; max-age=31536000;" //if no cookie policy is set default to no cookies
+        if(key !== "cookies" && parseCookies(document.cookie)["cookies"] === "false") throw new Error("cookies forbidden") //if trying to write with cookie policy false throw err
+        document.cookie = `${key}=${cookieEncode(value)}; max-age=31536000;` //write
+    },
+    exists: key => {
+        const settings = parseCookies(document.cookie)
+        return typeof settings[key] != "undefined"
+    },
+    remove: key => {
+        document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:01 GMT;`
+    },
+    get: key => {
+        return key ? cookieDecode(parseCookies(document.cookie)[key]) : parseCookies(document.cookie)
+    }
+}
+
+window.popup = (type,data) => {
+    const html = `<div class="centerDiv popup popup-${type}">
+    <h1 class="center">${type}</h1>
+    <p>${data}</p>
+    <button onclick='$(this).parent().remove()'>Dismiss</button>
+    </div>`
+    $("body").prepend(html)
+}
+
+$(document).ready(() => {
+    if(!window.setting.exists("cookies") || window.setting.get("cookies") == "false") {
+        $("body").prepend(`<div id="cookie_header" class="banner center"><span id="notice-text">this site uses cookies to store settings. By continuing to use the site you agree to the <a href="/terms">Terms of service</a></span><hr class="mobile"><button onclick="window.setting.set('cookies',true); $('#cookie_header').remove()" id="btn-dismiss" class="btn-yellow">dismiss</button></div>`)
+    }
+})
+
+/* handle uncought errors */
+window.onerror = (err) => {
+    window.popup("error",`${err}<br><a class="btn-red" href="https://stackoverflow.com/search?q=${err}">Stack overflow</a>`)
+}
